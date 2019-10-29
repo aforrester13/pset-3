7126f4c1a077cca1763cb4f91eaa484e5b2bdd5a
Aidan Forrester <AForrester@ucvts.tec.nj.us>
2019-10-29 13:15:17 -0400

    final commit

--- src/change.js
+++ src/change.js
@@ -3,12 +3,29 @@ const readlineSync = require("readline-sync");
 const MIN = 0.01;
 const MAX = 0.99;
 
+
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
+Q = 0.25
+D = 0.1
+N = 0.05
+P = 0.01
+
+
+
 if (Number.isNaN(amount)) {
-    // print your error message here
+    console.log("Invalid");
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+    console.log("Invalid");
 } else {
-    // write your code for making change here
+
+    let Quarters = Math.floor(amount/Q);
+    let amount1 = amount % Q;
+    let Dimes = Math.floor(amount1/D);
+    let amount2 = amount1 % D;
+    let Nickels = Math.floor(amount2/N);
+    let amount3 = amount2 % N;
+    let Pennies = Math.ceil(amount3/P);
+
+    console.log("\n" + Quarters + "quarters, " + Dimes + "dimes, " + Nickels + "nickels, and " + Pennies + "pennies." );
 }
