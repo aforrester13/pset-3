d12329fd08d14a7ed490dae9f6482d4ba6ca2c91
Aidan Forrester <Aidan@Daras-MacBook-Air.local>
2019-10-29 21:19:59 -0400

    final commit

--- src/sign.js
+++ src/sign.js
@@ -5,6 +5,12 @@ const theNumber = String(readlineSync.question("\nEnter a number: "));
 if (theNumber.includes(".")) {
    console.log("\nInvalid.\n")
 
+} else if (theNumber < (-9,007,199,254,740,992)) {
+   console.log("\nInvalid.\n")
+
+} else if (theNumber > 9,007,199,254,740,992) {
+   console.log("\nInvalid.\n")
+
 } else if (theNumber < 0) {
    console.log("\nNegative.\n")
 
