a7c6a8b4454ce8fbca596ec3df0c7b26ea4624df
Aidan Forrester <Aidan@Daras-MacBook-Air.local>
2019-10-29 21:10:15 -0400

    final commit

--- src/sign.js
+++ src/sign.js
@@ -1,14 +1,15 @@
 const readlineSync = require("readline-sync");
 
-const theNumber = Number(readlineSync.question("\nEnter a number: "));
+const theNumber = String(readlineSync.question("\nEnter a number: "));
 
-
-if (theNumber > 0) {
-   console.log("\nPositive\n")
+if (theNumber.includes(".")) {
+   console.log("\nInvalid\n")
 } else if (theNumber < 0) {
    console.log("\nNegative\n")
 } else if (theNumber === 0) {
    console.log("\nZero\n")
+} else if (theNumber > 0) {
+   console.log("\nPositive\n")
 } else {
    console.log("\nInvalid\n")
 }
